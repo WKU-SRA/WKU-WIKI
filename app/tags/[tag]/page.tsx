de4fb97ffeb5b162f77fd4d43df21e22f41f940e
AlbertZhaoCA <Zhaoq@kean.edu>
2024-07-23 10:44:46 +0800

    debug

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -32,13 +32,17 @@ export const generateStaticParams = async () => {
 
 export default function TagPage({ params }: { params: { tag: string } }) {
   const tag = decodeURI(params.tag)
-  console.log(params.tag)
-  console.log('tag:', tag)
   // Capitalize first letter and convert space to dash
   const title = tag[0].toUpperCase() + tag.split(' ').join('-').slice(1)
-  console.log(title)
   const filteredPosts = allCoreContent(
     sortPosts(allBlogs.filter((post) => post.tags && post.tags.map((t) => slug(t)).includes(tag)))
   )
-  return <ListLayout posts={filteredPosts} title={title} />
+  return (
+    <div>
+      {tag}
+      {params.tag}
+      {decodeURI(params.tag)}
+      <ListLayout posts={filteredPosts} title={title} />
+    </div>
+  )
 }
