191fcc2fc9792c686d129935e30631ac1639561f
AlbertZhaoCA <Zhaoq@kean.edu>
2024-08-09 11:42:43 +0800

    feat: Add ResizableWrapper component with slider for resizable behaviour
    - Created a new component `ResizableWrapper` that includes a slider to adjust the size of the wrapper dynamically.
    - The slider allows resizing the wrapper by ratio.
    
    This commit introduces a new feature for testing the resizable wrapper functionality, enhancing the flexibility of the component layout."

---
+++ components/ResizableModal.tsx
@@ -0,0 +1,39 @@
+'use client'
+import React, { useState, useRef, useEffect } from 'react'
+import Modal from './ui/modal'
+import Slider from './Slider'
+
+const ResizableModal = ({ element }) => {
+  const [sliderValue, setSliderValue] = useState(50)
+  const modalRef = useRef<HTMLDivElement>(null)
+  const [aspectRatio, setAspectRatio] = useState(1)
+
+  const handleChange = (value: number[]) => {
+    if (value.length > 0) {
+      setSliderValue(value[0])
+    }
+  }
+
+  useEffect(() => {
+    if (modalRef.current) {
+      const { width, height } = modalRef.current.getBoundingClientRect()
+      setAspectRatio(width / height)
+    }
+  }, [])
+
+  const modalWidth = `${sliderValue}%`
+  const modalHeight = `calc(${sliderValue}% / ${aspectRatio})`
+
+  return (
+    <div>
+      <Slider value={[sliderValue]} onValueChange={handleChange} />
+      <div ref={modalRef} style={{ width: modalWidth, height: modalHeight }}>
+        <Modal element={element} />
+      </div>
+      <p>Modal Width: {modalWidth}</p>
+      <p>Modal Height: {modalHeight}</p>
+    </div>
+  )
+}
+
+export default ResizableModal
