cc79c2c5fa72da211ddb69071d28bf882cd051b3
AlbertZhaoCA <Zhaoq@kean.edu>
2024-08-19 14:08:39 +0800

    feat: Update 'About Us' page to display all authors
    - Modify the page to map over all authors and extract core content
    - Render a grid layout to display each author's content dynamically
    
    This update makes every author's information shown.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,14 +7,24 @@ import { genPageMetadata } from 'app/seo'
 export const metadata = genPageMetadata({ title: '关于我们' })
 
 export default function Page() {
-  const author = allAuthors.find((p) => p.slug === 'default') as Authors
-  const mainContent = coreContent(author)
+  const mainContent = allAuthors.map((author) => {
+    return { content: coreContent(author), author }
+  })
 
   return (
     <div className="flex flex-col">
-      <AuthorLayout content={mainContent}>
-        <MDXLayoutRenderer code={author.body.code} />
-      </AuthorLayout>
+      <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          关于我们
+        </h1>
+      </div>
+      <div className="grid grid-flow-row grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+        {mainContent.map(({ content, author }) => (
+          <AuthorLayout key={author._id} content={content}>
+            <MDXLayoutRenderer code={author.body.code} />
+          </AuthorLayout>
+        ))}
+      </div>
     </div>
   )
 }
