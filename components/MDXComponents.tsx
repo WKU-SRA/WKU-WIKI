d0056f9933a295de6510080608895dacaf3085de
AlbertZhaoCA <Zhaoq@kean.edu>
2024-08-31 15:45:48 +0800

    debug: resolve conflict between React components and HTML elements
    - Changed all react component alias from element to Element
    - Ensured proper capitalization to distinguish React components from HTML elements
    - Updated component references accordingly in data/blog

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,14 +1,33 @@
 import TOCInline from 'pliny/ui/TOCInline'
 import Pre from 'pliny/ui/Pre'
 import BlogNewsletterForm from 'pliny/ui/BlogNewsletterForm'
+import Divider from '@/components/ui/divider'
 
 import type { MDXComponents } from 'mdx/types'
 import Image from './Image'
 import CustomLink from './Link'
 import TableWrapper from './TableWrapper'
+import Quiz from 'components/Quiz'
+import CodeBlock from 'components/Code'
+import VideoPlayer from 'components/Player'
+import { TimeStampPlayer } from 'components/Player'
+import ImageWrapper from 'components/ImageWrapper'
+import Modal from 'components/ui/modal'
+import Quote from 'components/QuoteWrapper'
+import FrameModal from 'components/IframeWrapper'
+import Player from 'components/Player'
 
-//not used, I did not find the usage of this file, so I deprecated it. see @ app/.../blog/
 export const components: MDXComponents = {
+  Frame: FrameModal,
+  Quote,
+  Modal,
+  Quiz,
+  CodeBlock,
+  TSplayer: TimeStampPlayer,
+  Player,
+  Divider,
+  ImageWrapper,
+  VideoPlayer,
   Image,
   TOCInline,
   a: CustomLink,
