7ca85586c6ff4be42fb5f39576750f03e4414004
AlbertZhaoCA <Zhaoq@kean.edu>
2024-09-08 12:48:35 +0800

    feat: Make the navbar fixed and scrollable
    - Updated the Header component to have a fixed position.
    - Ensured that the navbar can scroll together with the page content.
    - Adjusted styles to maintain proper layout and responsiveness.
    
    This change improves the user experience by keeping the navbar accessible while scrolling through the page.

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,10 +5,12 @@ import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
+import { User2Icon } from 'lucide-react'
+import { Avatar, AvatarFallback, AvatarImage } from '@radix-ui/react-avatar'
 
 const Header = () => {
   return (
-    <header className="fixed left-0 right-0 top-0 z-50 bg-white shadow-md dark:bg-slate-900">
+    <header className="fixed left-0 right-0 top-0 z-50 bg-white opacity-85 shadow-md dark:bg-slate-900">
       <div className="flex items-center justify-around px-6 py-4">
         <div>
           <Link href="/" aria-label={siteMetadata.headerTitle}>
@@ -38,6 +40,14 @@ const Header = () => {
                 {link!.title}
               </Link>
             ))}
+          <Link href="/login">
+            <Avatar>
+              <AvatarImage width={48} height={48} src="" alt="user" />
+              <AvatarFallback>
+                <User2Icon className="hover:text-primary-500" />
+              </AvatarFallback>
+            </Avatar>
+          </Link>
           <SearchButton />
           <ThemeSwitch />
           <MobileNav />
